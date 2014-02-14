540aea1f8e291d153126082d10f05b0b69b2043d
Maxfield Chen <madhatterde@gmail.com>
2014-02-14 08:22:50 -0800

    Began Messing around with Node.js. Hopefully this can be used for the multiplayer aspect, but right now this just creates a simple http server and spits out hello world into an html document.

---
+++ Node/Server.js
@@ -0,0 +1,10 @@
+var http = require("http");
+function start (){
+http.createServer(function(request, response) {
+	console.log('Request Recieved.')
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World");
+	response.end();
+}).listen(8888);
+console.log('Server Started');
+}
